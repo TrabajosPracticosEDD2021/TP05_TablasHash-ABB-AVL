242df559811d91b11e968575ebf743c458dd2df6
Kevin D. Kling <uner.kevin.kling@outlook.es>
2021-11-06 16:58:07 -0300

    Resuelve el punto 3 y 4 del tp (ABB)

--- src/main/java/b_abb/ArbolABBExt.java
+++ src/main/java/b_abb/ArbolABBExt.java
@@ -1,29 +1,129 @@
 package b_abb;
 
 import ar.edu.uner.fcad.ed.arbolesabbyavl.ArbolABB;
+import ar.edu.uner.fcad.ed.arbolesabbyavl.ArbolABBResultadoBusqueda;
 import ar.edu.uner.fcad.ed.arbolesabbyavl.NodoABB;
+import ar.edu.uner.fcad.ed.edlineales.ListaEnlazadaNoOrdenada;
 
 public class ArbolABBExt<T extends Comparable<? super T>> extends ArbolABB<T>
 		implements ArbolABBExtInterfaz<T> {
 
+	/** Devuelve el valor del nodo con menor valor de clave. */
 	public T min() {
-		// TODO Auto-generated method stub
-		return null;
+		NodoABB<T> nodoAux = raiz;
+
+		while (nodoAux.tieneHijoIzquierdo()) {
+			nodoAux = nodoAux.getHijoIzquierdo();
+		}
+
+		return nodoAux.getValor();
 	}
 
+	/** Devuelve el valor del nodo con mayor valor de clave. */
 	public T max() {
-		// TODO Auto-generated method stub
-		return null;
+		NodoABB<T> nodoAux = raiz;
+
+		while (nodoAux.tieneHijoDerecho()) {
+			nodoAux = nodoAux.getHijoDerecho();
+		}
+
+		return nodoAux.getValor();
 	}
 
+	/**
+	 * Elimina la rama del árbol que tiene como raíz el nodo pasado por parámetro.
+	 */
 	public void eliminarRama(NodoABB<T> nodo) {
-		// TODO Auto-generated method stub
+		ArbolABBResultadoBusqueda<T> buscado = nodosBuscar(nodo.getValor());
 
+		while (buscado.getNodoBuscado().getCantidadHijos() != 0) {
+			if (buscado.getNodoBuscado().tieneHijoIzquierdo()) {
+				borrarNodo(buscado.getNodoBuscado(), buscado.getNodoBuscado().getHijoIzquierdo());
+			} else if (buscado.getNodoBuscado().tieneHijoDerecho()) {
+				borrarNodo(buscado.getNodoBuscado(), buscado.getNodoBuscado().getHijoDerecho());
+			}
+		}
+
+		borrarNodo(buscado.getPadre(), buscado.getNodoBuscado());
 	}
 
+	private ArbolABBResultadoBusqueda<T> nodosBuscar(T valor) {
+		ArbolABBResultadoBusqueda<T> resultado = new ArbolABBResultadoBusqueda<T>();
+
+		NodoABB<T> nodoAuxPadre = null;
+		NodoABB<T> nodoAux = raiz;
+
+		while (nodoAux != null) {
+			if (valor.compareTo(nodoAux.getValor()) == 0) {
+				break;
+			} else {
+				if (valor.compareTo(nodoAux.getValor()) < 0) {
+					nodoAuxPadre = nodoAux;
+					nodoAux = nodoAux.getHijoIzquierdo();
+				} else {
+					nodoAuxPadre = nodoAux;
+					nodoAux = nodoAux.getHijoDerecho();
+				}
+			}
+		}
+
+		resultado.setPadre(nodoAuxPadre);
+		resultado.setNodoBuscado(nodoAux);
+
+		return resultado;
+	}
+
+	/**
+	 * Devuelve el ancestro compartido por los nodos x e y que se encuentra más
+	 * lejos de la raíz (más profundo). En caso que alguno de los nodos no
+	 * pertenezca al árbol devuelve null.
+	 */
 	public NodoABB<T> menorAntecesorComun(NodoABB<T> x, NodoABB<T> y) {
-		// TODO Auto-generated method stub
-		return null;
+
+		if (!exists(x.getValor()) || !exists(y.getValor())) {
+			return null;
+		}
+
+		ListaEnlazadaNoOrdenada<NodoABB<T>> ancestrosX = listAncestros(x);
+		ListaEnlazadaNoOrdenada<NodoABB<T>> ancestrosY = listAncestros(y);
+		NodoABB<T> resultado = ancestrosX.first();
+		boolean encontrado = false;
+
+		while (!encontrado) {
+			if (ancestrosY.contains(resultado)) {
+				encontrado = true;
+			} else {
+				ancestrosX.removeFirst();
+				resultado = ancestrosX.first();
+			}
+		}
+
+		return resultado;
 	}
 
+	/**
+	 * Retorna todos los ancestros del nodo pasado por parametro
+	 *
+	 */
+	private ListaEnlazadaNoOrdenada<NodoABB<T>> listAncestros(NodoABB<T> nodo) {
+		ListaEnlazadaNoOrdenada<NodoABB<T>> resultado = new ListaEnlazadaNoOrdenada<NodoABB<T>>();
+		NodoABB<T> nodoAux = raiz;
+		T valor = nodo.getValor();
+
+		while (nodoAux != null) {
+			if (valor.compareTo(nodoAux.getValor()) == 0) {
+				break;
+			} else {
+				if (valor.compareTo(nodoAux.getValor()) < 0) {
+					resultado.addToFront(nodoAux);
+					nodoAux = nodoAux.getHijoIzquierdo();
+				} else {
+					resultado.addToFront(nodoAux);
+					nodoAux = nodoAux.getHijoDerecho();
+				}
+			}
+		}
+
+		return resultado;
+	}
 }
--- src/main/java/b_abb/ClienteArbolABBExt.java
+++ src/main/java/b_abb/ClienteArbolABBExt.java
@@ -1,10 +1,12 @@
 package b_abb;
 
+import ar.edu.uner.fcad.ed.arbolesabbyavl.NodoABB;
 import ar.edu.uner.fcad.ed.edlineales.iteradores.Iterador;
 
 public class ClienteArbolABBExt {
 	public static void main(String[] args) {
 		ArbolABBExt<Integer> arbolito = new ArbolABBExt<Integer>();
+		// Arbol que esta en la filmina 12 de ABB.
 		arbolito.add(12);
 		arbolito.add(7);
 		arbolito.add(21);
@@ -14,11 +16,29 @@ public class ClienteArbolABBExt {
 		arbolito.add(25);
 		arbolito.add(2);
 		arbolito.add(8);
-		Iterador <Integer> itedarorcito = arbolito.iteradorPorNiveles();
+		arbolito.add(13);
+		arbolito.add(19);
+
+//		 Imprime por niveles
+		Iterador<Integer> itedarorcito1 = arbolito.iteradorPorNiveles();
+		while (itedarorcito1.existeSiguiente()) {
+			System.out.println(itedarorcito1.siguiente());
+		}
+
+		System.out.println("\narbolito.min() : " + arbolito.min());
+
+		System.out.println("\narbolito.max() : " + arbolito.max());
+
+		System.out.println("\nmenorAntecesorComun(13, 25): "
+				+ arbolito.menorAntecesorComun(new NodoABB<Integer>(13), new NodoABB<Integer>(25))
+						.getValor());
+
+		System.out.println("\narbolito.eliminarRama(21):");
+		arbolito.eliminarRama(new NodoABB<Integer>(21));
+//		 Imprime por niveles
+		Iterador<Integer> itedarorcito = arbolito.iteradorPorNiveles();
 		while (itedarorcito.existeSiguiente()) {
 			System.out.println(itedarorcito.siguiente());
 		}
-		
-		
 	}
 }
